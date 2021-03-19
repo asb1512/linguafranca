4a4181202867248063e0edf1ff2a19787ac7c103
asb1512 <andrew@olympicmedia.com>
2021-03-18 22:49:45 -0400

    Updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-   
+   has_secure_password
+   has_one_attached :profile_pic
+   has_many :invitations
+   has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
+   has_many :foreign_languages, through: :foreign_languages_users
+   has_many :native_languages, through: :native_languages_users
 end
