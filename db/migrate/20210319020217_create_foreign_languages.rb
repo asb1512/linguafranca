4a4624e18c9989c40d6d93819ea85842f2de6311
asb1512 <andrew@olympicmedia.com>
2021-03-18 22:22:47 -0400

    Set up foreign_languages table

--- db/migrate/20210319020217_create_foreign_languages.rb
+++ db/migrate/20210319020217_create_foreign_languages.rb
@@ -1,6 +1,10 @@
 class CreateForeignLanguages < ActiveRecord::Migration[6.1]
   def change
     create_table :foreign_languages do |t|
+      t.string            :name
+      t.string            :wikipedia_link
+      t.boolean           :approved, default: false
+      t.boolean           :pitched
 
       t.timestamps
     end
