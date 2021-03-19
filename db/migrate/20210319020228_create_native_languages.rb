355e847661c480b7d2f59cfaa7c27f6dfc808870
asb1512 <andrew@olympicmedia.com>
2021-03-18 22:27:37 -0400

    set up native languages table

--- db/migrate/20210319020228_create_native_languages.rb
+++ db/migrate/20210319020228_create_native_languages.rb
@@ -1,6 +1,11 @@
 class CreateNativeLanguages < ActiveRecord::Migration[6.1]
   def change
     create_table :native_languages do |t|
+      t.string            :name
+      t.string            :wikipedia_link
+      t.boolean           :approved, default: false
+      t.boolean           :pitched
+      t.boolean           :gendered
 
       t.timestamps
     end
