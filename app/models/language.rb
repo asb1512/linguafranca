eb450c974a817b4e38616491ba3f925e4690095c
asb1512 <andrew@olympicmedia.com>
2021-03-21 00:06:25 -0400

    Fixed AR associations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,5 @@
 class Language < ActiveRecord::Base
-   has_many :users
-   has_many :users, through: :languages_users
+   has_many :language_users
+   has_many :users, through: :language_users
    has_and_belongs_to_many :flags
 end
