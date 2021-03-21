86689aa74fd1ff61fc332ce9be60d1c88a9a7446
asb1512 <andrew@olympicmedia.com>
2021-03-20 21:27:28 -0400

    updated User model ActiveRecord associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
    has_one_attached :profile_pic
    has_many :invitations
    has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
-   has_many :foreign_languages, through: :foreign_languages_users
-   has_many :native_languages, through: :native_languages_users
+   has_many :languages
+   has_many :languages, through: :languages_users
+   serialize :foreign_language_ids, Array
+   serailize :native_language_ids, Array
 end
