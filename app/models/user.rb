d367949a9dd48bb656edfa3dbba6b3f2ad17f233
asb1512 <andrew@olympicmedia.com>
2021-03-21 00:06:06 -0400

    fixed error with AR associations' 'has many through' relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
    has_one_attached :profile_pic
    has_many :invitations
    has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
-   has_many :languages
-   has_many :languages, through: :languages_users
+   has_many :language_users
+   has_many :languages, through: :language_users
 end
