4801c29450bd798c3fee1c634175284bac5155c7
asb1512 <andrew@olympicmedia.com>
2021-03-21 15:40:29 -0400

    added to users_params to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
    end
 
    def create
-      
+
+   end
+
+   private
+
+   def user_params
+      params.require(:user).permit(
+         :email,
+         :password,
+         :password_confirmation,
+         :username,
+         :first_name,
+         :last_name,
+         :age,
+         :skype_username
+      )
    end
 end
