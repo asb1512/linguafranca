9925afaef81d64c549b380267e6d10950e587c30
asb1512 <andrew@olympicmedia.com>
2021-03-22 10:34:05 -0400

    Added model method to allow the display of user's native language

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
    has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
    has_many :language_users
    has_many :languages, through: :language_users
+
+   def native_language
+      self.language_users.where({foreign_language: false}).each do |lang|
+         result = Language.find_by(id: lang.language_id)
+         result.each do |e|
+            result
+         end
+      end
+   end
 end
