7d695ef8b009dbbcbd9c8341f43a7d9306daeb3e
asb1512 <andrew@olympicmedia.com>
2021-03-22 12:13:41 -0400

    working on adding authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+   protect_from_forgery with: :exception
+   before_action :verified_user
+   helper_method :current_user
+
+   private
+
+   def verified_user
+      redi
+   end
 end
