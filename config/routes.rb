fea28253fc75ba488646780f1ca0d74a05f12eb3
asb1512 <andrew@olympicmedia.com>
2021-03-22 12:13:18 -0400

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static#home'
+
   get '/signup', to: 'users#new'
   resources :users, only: [:create, :show, :edit, :update, :destroy]
+  
   resources :languages
 end
