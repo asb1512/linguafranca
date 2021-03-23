09b2dae008dce800a516a5f4673dc70b81a3b3e8
asb1512 <andrew@olympicmedia.com>
2021-03-23 10:52:39 -0400

    added validations and new, create, and delete actions for SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController
-   
+   skip_before_action :verified_user, only: [:new, :create]
+
+   def new
+      @user = User.new
+   end
+
+   def create
+      if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to user_path(@user)
+      else
+         render 'new'
+      end
+   end
+
+   def destroy
+      
+   end
 end
