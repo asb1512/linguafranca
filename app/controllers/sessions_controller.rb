31f2ddf4b4c0dd6de69eb188749bb10c7eeab77c
asb1512 <andrew@olympicmedia.com>
2021-03-23 16:08:06 -0400

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,16 @@ class SessionsController < ApplicationController
    skip_before_action :verified_user, only: [:new, :create]
 
    def new
-      @user = User.new
+      if current_user
+         redirect_to user_path(current_user)
+      else
+         @user = User.new
+      end
    end
 
    def create
-      if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
          session[:user_id] = @user.id
          redirect_to user_path(@user)
       else
@@ -15,6 +20,6 @@ class SessionsController < ApplicationController
    end
 
    def destroy
-      
+
    end
 end
