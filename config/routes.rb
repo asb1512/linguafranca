55d6a2e17011749646db1c6a748b06611cd78e60
asb1512 <andrew@olympicmedia.com>
2021-03-23 16:02:10 -0400

    changed 'signin' post route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :languages
 
   get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
+  post '/sessions', to: 'sessions#create'
 end
