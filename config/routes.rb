f105b8949ebb32d491129c4371cbdb8a975efcf3
asb1512 <andrew@olympicmedia.com>
2021-03-24 15:32:48 -0400

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :languages
 
   get '/signin', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
+  post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
