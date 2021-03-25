8805a26e723c40878a29f748284fbba1a6ef6b2b
asb1512 <andrew@olympicmedia.com>
2021-03-25 14:20:07 -0400

    constructing methods that will allow app to find users that speak the language that the user is learning

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,26 @@ class User < ActiveRecord::Base
       results
    end
 
+   def native_language_id
+      self.native_languages.first.id
+   end
+
    def foreign_languages
       results = []
-      language_users = self.language_users.where({foreign_language: true})
+      language_users = self.language_users.where(foreign_language: true)
       language_users.each do |e|
          result = Language.find_by(id: e.language_id)
          results << result
       end
       results
+   end
+
+   def foreign_language_id
+      self.foreign_languages.first.id
+   end
+
+   def matching_users
+   #Find users 
+      LanguageUser.all.where(language_id: current_user.foreign_language_id).where().where(foreign_language: false)
    end
 end
