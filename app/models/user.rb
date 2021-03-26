8854ceca4d17cb20eb50551f49e3c6b8e8ebc413
asb1512 <andrew@olympicmedia.com>
2021-03-26 14:10:08 -0400

    added 3 methods that allows pertinent info for invitations model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
    validates :email, :password, :username, :first_name, :last_name, :age, :skype_username, presence: true
 
+   #methods for retrieving a user's language info
    def native_languages
       results = []
       language_users = self.language_users.where({foreign_language: false})
@@ -45,5 +46,21 @@ class User < ActiveRecord::Base
          end
       end
       results
+   end
+
+   #methods dealing with invitations (friend requests)
+   def friends
+      friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
+      friends_i_got_invitation = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
+      ids = friends_i_sent_invitation + friends_i_got_invitation
+      User.where(id: ids)
+   end
+
+   def friend_with?(user)
+      Invitation.confirmed_record?(id, user.id)
+   end
+
+   def send_invitation(user)
+      invitations.create(friend_id: user.id)
    end
 end
