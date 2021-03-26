17f530b86365ab07c46a0897cbf42e8bb260e580
asb1512 <andrew@olympicmedia.com>
2021-03-26 11:57:43 -0400

    upated invitations#create

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,10 @@
 class InvitationsController < ApplicationController
    def create
-      raise params.inspect
+      invitation = Invitation.create(user_id: current_user.id, friend_id: params[:id])
+      if invitation
+         redirect_to user_path(params[:id])
+      else
+         redirect_to user_path(current_user)
+      end
    end
 end
