1f094b9925a6115a1d4e1a302eb64aabe0d549d4
asb1512 <andrew@olympicmedia.com>
2021-03-26 16:50:50 -0400

    added instance method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
 
    def show
       @user = User.find_by(id: params[:id])
+      @pending_invitations = @user.pending_invitations
    end
 
    def home
