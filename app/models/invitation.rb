77702e9f3c401a5264efa3bd2d1a4124dfa8959c
asb1512 <andrew@olympicmedia.com>
2021-03-26 14:10:21 -0400

    added methods to Invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,13 @@
 class Invitation < ActiveRecord::Base
    belongs_to :user
+
+   def self.reacted?(id1, id2)
+      case1 = !Invitation.where(user_id: id1, friend_id: id2).empty?
+      case2 = !Invitation.where(user_id: id2, friend_id: id1).empty?
+      case1 || case2
+   end
+
+   def self.confirmed_record?(id1, id2)
+      case1 = !Invitation.where(user_id: id1, friend_id: id2, confirmed:true).empty?
+   end
 end
