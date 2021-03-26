fe9324ae23c3335e2b0ee23218c74ab49d501fc7
asb1512 <andrew@olympicmedia.com>
2021-03-26 11:35:08 -0400

    added POST route for Invitation model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #static routes
   root 'static#welcome'
   get '/about', to: 'static#about'
 
+  #user routes
   get '/signup', to: 'users#new'
   resources :users, only: [:create, :show, :edit, :update, :destroy]
   get '/home', to: 'users#home'
   
+  #language routes
   resources :languages
 
+  #session routes
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  #invitation routes
+  post '/invite/:id', to: 'invitations#create', as: 'invite'
 end
