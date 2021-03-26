095c369189d6c54ff20f86eff7b6a2c3b859c9c4
asb1512 <andrew@olympicmedia.com>
2021-03-26 11:05:55 -0400

    changed the /home route's action to users#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#welcome'
-  get '/home', to: 'static#home'
   get '/about', to: 'static#about'
 
   get '/signup', to: 'users#new'
   resources :users, only: [:create, :show, :edit, :update, :destroy]
+  get '/home', to: 'users#home'
   
   resources :languages
 
