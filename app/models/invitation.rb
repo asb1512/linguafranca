52e488e014400be8ccc532b860fc7d5100e044cf
asb1512 <andrew@olympicmedia.com>
2021-03-26 16:49:08 -0400

    added more helper methods to Invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -9,5 +9,15 @@ class Invitation < ActiveRecord::Base
 
    def self.confirmed_record?(id1, id2)
       case1 = !Invitation.where(user_id: id1, friend_id: id2, confirmed:true).empty?
+      case2 = !Invitation.where(user_id: id2, friend_id: id1, confirmed: true).empty?
+      case1 || case2
+   end
+
+   def self.find_invitation(id1, id2)
+      if Invitation.where(user_id: id1, friend_id: id2, confirmed: true).empty?
+         Invitation.where(user_id: id2, friend_id: id1, confirmed: true)[0].id
+      else
+         Invitation.where(user_id: id1, friend_id: id2, confirmed: true)[0].id
+      end
    end
 end
