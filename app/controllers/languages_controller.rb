1ad86e71b9178bbe37781c7212f7f1dbe3084e05
asb1512 <andrew@olympicmedia.com>
2021-03-31 16:48:56 -0400

    added validations, edit, update and strong params to LanguagesController

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,5 +1,24 @@
 class LanguagesController < ApplicationController
+   before_action :admin?
+   skip_before_action :admin?, only: [:show]
+   
    def show
       @language = Language.find_by(id: params[:id])
+   end
+
+   def edit
+      @language = Language.find_by(id: params[:id])
+   end
+
+   def update
+      language = Language.find_by(id: params[:id])
+      language.update(language_params)
+      redirect_to language_path(language.id)
+   end
+
+   private
+
+   def language_params
+      params.require(:language).permit(:name, :pitched, :gendered, :wikipedia_link)
    end
 end
