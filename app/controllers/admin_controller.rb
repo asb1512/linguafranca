b941bf7e0e0e919f65143c20c9e8322dcfcb106f
asb1512 <andrew@olympicmedia.com>
2021-03-31 22:22:34 -0400

    added users#create_user and private user_params method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,4 +10,27 @@ class AdminController < ApplicationController
    def new_user
       @user = User.new
    end
+
+   def create_user
+      if user = User.create(user_params)
+   end
+
+   private
+
+   def user_params
+      params.require(:user).permit(
+         :email,
+         :password,
+         :password_confirmation,
+         :username,
+         :first_name,
+         :last_name,
+         :age,
+         :skype_username,
+         :native_language_id,
+         :foreign_language_id,
+         :profile_pic,
+         :admin
+      )
+   end
 end
