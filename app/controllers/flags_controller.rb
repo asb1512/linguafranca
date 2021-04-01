c42e8ce508bfa6e1f326f986c8edec16ad6ea471
asb1512 <andrew@olympicmedia.com>
2021-04-01 13:18:25 -0400

    added new, create actions and created #flag_params

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -2,7 +2,23 @@ class FlagsController < ApplicationController
    before_action :admin?
    skip_before_action :admin?, only: [:show]
 
+   def new
+      @flag = Flag.new
+   end
+
+   def create
+      if flag = Flag.create(flag_params)
+
+      end
+   end
+
    def show
       @flag = Flag.find_by(id: params[:id])
+   end
+
+   private
+
+   def flag_params
+      params.require(:flag).permit(:nationality, :wikipedia_link, :name, :approved)
    end
 end
