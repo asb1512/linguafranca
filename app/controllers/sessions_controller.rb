175215e606988cc524fba2366a39426ebc0d2eec
asb1512 <andrew@olympicmedia.com>
2021-04-07 12:34:58 -0400

    updated sessions#github_oauth to handle user creation when logging in from GitHub

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,9 +22,24 @@ class SessionsController < ApplicationController
 
    def github_oauth
       @omniauth_data = request.env['omniauth.auth']
-      raise @omniauth_data.inspects
+      
       if @user = User.find_by(github_id: @omniauth_data["uid"]).where(github: true)
          redirect_to user_path(@user.id)
+      else
+         user_full_name = @omniauth_data["info"]["name"].split
+         
+         @user = User.create(
+            email: "none",
+            password: @omniauth_data["uid"],
+            username: @omniauth_data["info"]["nickname"],
+            first_name: user_full_name[0],
+            last_name: user_full_name[1],
+            age: 0,
+            skype_username: "none",
+            github: true,
+            github_id: @omniauth_data["uid"]
+         )
+         redirect_to edit_user_path(@user.id)
       end
    end
 
