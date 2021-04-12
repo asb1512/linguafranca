02f7cbdc051103d730f7653eb0012ee0253f1fba
asb1512 <andrew@olympicmedia.com>
2021-04-12 12:29:16 -0400

    added validations to Language model

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,4 +2,6 @@ class Language < ActiveRecord::Base
    has_many :language_users
    has_many :users, through: :language_users
    has_and_belongs_to_many :flags
+   
+   validates :name, :wikipedia_link, presence: true
 end
