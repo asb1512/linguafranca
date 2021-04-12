22e7a7c2572dd48e2fe4f2930290b7ca1e16396e
asb1512 <andrew@olympicmedia.com>
2021-04-12 15:51:00 -0400

    added validation to Flag model for uniqueness

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -2,5 +2,5 @@ class Flag < ActiveRecord::Base
    has_and_belongs_to_many :languages
    has_one_attached :flag_pic
 
-   validates :nationality, :wikipedia_link, :name, presence: true
+   validates :nationality, :wikipedia_link, :name, presence: true, uniqueness: true
 end
